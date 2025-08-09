901fc9c32fdc56f399e53dd5eb3c635a96691448
mdmonauwarulislam <mdmonauwarulislam@gmail.com>
2025-08-10 00:51:50 +0530

    fix cors issue

--- server/server.js
+++ server/server.js
@@ -15,7 +15,13 @@ const prisma = new PrismaClient();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://store-rating-app-pi.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
