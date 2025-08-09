ca14d5c7b433cb2e5f05ef16099d752b7c1d6859
mdmonauwarulislam <mdmonauwarulislam@gmail.com>
2025-08-09 23:49:33 +0530

    add vercel.json and update backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://store-rating-app-1-m6x4.onrender.com',
+        target: 'https://store-rating-app-1-m6x4.onrender.com/',
         changeOrigin: true
       }
     }
