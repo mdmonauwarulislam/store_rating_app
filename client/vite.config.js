529b6c93648d222356a714b01a5f92e2acc5501c
mdmonauwarulislam <mdmonauwarulislam@gmail.com>
2025-08-09 23:15:03 +0530

    upadte live backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://store-rating-app-1-m6x4.onrender.com',
         changeOrigin: true
       }
     }
